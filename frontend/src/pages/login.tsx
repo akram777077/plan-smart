e8a5408039611fdb5fac24d85b14c900752af867
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-03-17 00:39:31 +0100

    feat: create `login.tsx` component as the main login page
    - Login.tsx: Established the main login page component.
    - Layout: Designed a dark-themed background with a flexbox structure for alignment.
    - Responsiveness: Ensured the layout adapts seamlessly to different screen sizes.
    - Integration: Combined LoginHeader and LoginBody components into a cohesive login page.

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,10 +1,15 @@
-function Login() {
+import LoginHeader from "../components/pages/login/login-header";
+import LoginBody from "../components/pages/login/login-body";
 
+const Login = () => {
   return (
-    <>
-      LOGIN PAGE
-    </>
-  )
-}
+    <div className="bg-[#171a1f] min-h-screen flex items-center justify-center p-4">
+      <div className="max-w-6xl w-full flex flex-col md:flex-row items-center justify-between gap-8">
+        <LoginHeader />
+        <LoginBody />
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
